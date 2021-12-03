fca6b6fcd33b29a7fbea00238c120c37fe462745
Bo Chen <bochen1993cs@gmail.com>
2021-12-02 23:52:23 -0600

    Update waymo_pytorch_dataset.py

--- waymo_pytorch_dataset.py
+++ waymo_pytorch_dataset.py
@@ -267,7 +267,7 @@ if __name__ == '__main__':
     model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5m, yolov5l, yolov5x, custom
 
     # transform
-    thimg = transforms.ToTensor(image)
+    thimg = transforms.ToTensor()(image)
     # Inference
     results = model(thimg)
 
