f8a79722d60dd9a9fc2a9c7fd4b92bdbaa3d660c
Bo Chen <bochen1993cs@gmail.com>
2021-12-02 20:43:37 -0600

    Update waymo_pytorch_dataset.py

--- waymo_pytorch_dataset.py
+++ waymo_pytorch_dataset.py
@@ -247,7 +247,7 @@ if __name__ == '__main__':
     import cv2
     for obj in target:
         left,top,right,bottom = obj.box2d
-        if height <=20: continue
+        if bottom-top <=20: continue
         cv2.rectangle(image,(left, top), (right, bottom),(0,255,0))
         cv2.putText(image,obj.cls_type,(left,top),0,0.3,(0,255,0))
     cv2.imwrite('gt.jpg', image)
