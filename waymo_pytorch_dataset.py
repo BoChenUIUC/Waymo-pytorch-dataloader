7aff5460552ba0dcfa0da531cfe93ac4bae19a81
Bo Chen <bochen1993cs@gmail.com>
2021-12-02 18:58:25 -0600

    Update waymo_pytorch_dataset.py

--- waymo_pytorch_dataset.py
+++ waymo_pytorch_dataset.py
@@ -236,7 +236,7 @@ if __name__ == '__main__':
     LOCATIONS = ['location_sf']
 
     dataset = WaymoDataset(DATA_PATH, LOCATIONS, 'train', True, "new_waymo")
-    print(dataset.count_frames())
+    dataset.count_frames()
 
     for i in range(10):
         frame, idx = dataset.data, dataset.count
@@ -245,4 +245,4 @@ if __name__ == '__main__':
         target = dataset.get_label(frame, idx)
         image = dataset.get_image(frame, idx)
         
-        print(image.shape, idx, target)
+        print(image.shape, idx, target, pts, calib)
